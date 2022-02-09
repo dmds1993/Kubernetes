eeea8ea204c92b9e4a79e10ce46b9b549f3190f3
Ubuntu <DanielSantos@linux-pereiras-v3.baav2ez3hpoe5hsaa5qmva3ahc.nx.internal.cloudapp.net>
2022-02-09 03:01:09 +0000

    criado service kind NodePort

---
+++ services/WebApiCore/Controllers/HealthCheckController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApiCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<HealthCheckController> _logger;
+
+        public HealthCheckController(ILogger<HealthCheckController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("healthy")]
+        public IEnumerable<WeatherForecast> GetHealthy()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("ready")]
+        public IEnumerable<WeatherForecast> GetReady()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}

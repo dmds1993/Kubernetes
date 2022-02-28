d8c0092b8ba31746550495960bd7c3978896a682
Ubuntu <DanielSantos@linux-pereiras-v3.baav2ez3hpoe5hsaa5qmva3ahc.nx.internal.cloudapp.net>
2022-02-28 02:57:57 +0000

    incluido node e job

---
+++ job/JobApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JobApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Thread.Sleep(10000);
+            Console.WriteLine("Bye");
+        }
+    }
+}
